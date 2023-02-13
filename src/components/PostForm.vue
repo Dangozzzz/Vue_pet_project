5c0114c50039169265d8263d0d1ea2cf9275dd53
Dangoz <94177642+Dangozzzz@users.noreply.github.com>
2023-02-13 12:42:46 +0300

    Add files via upload

---
+++ src/components/PostForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+    
+    <!-- 2 вариант создания синхронизации с input       @input="title = $event.target.value" -->
+    <input 
+      v-bind:value="title"
+      @input="title = $event.target.value"
+      type="text" 
+      class="input" 
+      placeholder="Название">
+     
+
+     <!-- 2 вариант создания синхронизации с input       @input="body = $event.target.value"  (первый находится в script)-->
+    <input 
+      v-bind:value="body"  
+      @input="body = $event.target.value"
+      type="text" 
+      class="input" 
+      placeholder="Описание">
+    
+    
+    <button 
+      class="btn" >Создать</button>
+
+    </form>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+form{
+  display: flex;
+  flex-direction: column;
+  
+}
+.input{
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn{
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
