f4005e9538033ed2d2863e127312592372f86dd5
Dangoz <94177642+Dangozzzz@users.noreply.github.com>
2023-02-13 15:54:41 +0300

    Add files via upload

---
+++ project_vue/src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <!-- <div> -->
+
+  <!-- Динамическое добавление постов -->
+  <div class="app">
+
+
+
+ 
+<!-- КОРОТКАЯ ЗАПИСЬ :posts  ПОЛНАЯ ЗАПИСЬ v-bind:posts -->
+     <post-form  @create="createPost"/>
+     <post-list :posts="posts"/>
+
+    <!-- Тут была работа с массивами 22:41 -->
+    <!-- <div> <strong> Название:</strong> Пост о JavaScript</div>
+    <div> <strong> Описание:</strong> JavaScript</div> -->
+    <!-- Можно дублировать так или Создать массив постов -->
+    <!-- </div>
+    <div class="post">
+    <div> <strong> Название:</strong> Пост о JavaScript</div>
+    <div> <strong> Описание:</strong> JavaScript</div>
+    </div>
+    <div class="post">
+    <div> <strong> Название:</strong> Пост о JavaScript</div>
+    <div> <strong> Описание:</strong> JavaScript</div>
+    </div> -->
+
+
+
+
+
+    <!-- <div> -->
+    <!-- <button @click="addLike">Likes</button>
+    <button @click="addDislike">Dislike</button>
+    </div>
+    <div>Кол-во лайков <strong>{{ likes }}</strong> </div>
+    <div>Кол-во дилайков <strong>{{ dislikes }}</strong> </div> -->
+
+
+  </div>
+</template>
+
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+
+export default {
+  
+  components: {
+    PostList, PostForm,
+  },
+
+  data() {
+    return {
+      posts: [
+        { id: 1, title: " javaScript", body: "Описание поста" },
+        { id: 2, title: " javaScript", body: "Описание поста 2" },
+        { id: 3, title: " javaScript", body: "Описание поста 3" },
+      ],
+    };
+  },
+
+
+  methods: {
+    // Двустороннее связывание 
+    createPost(post){
+      this.posts.push(post);
+  
+
+          },
+// Синхронизаия модели с input
+// inputTitle(event){
+//   this.title = event.target.value;
+// }
+
+
+
+    // Тут был метод создания лайков 10:15
+    // addLike(){
+    //   this.likes += 1;
+    // },
+    // addDislike(){
+    //   this.dislikes += 1;
+    // },
+  },
+};
+</script>
+
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app{
+  padding: 20px;
+}
+</style>
---
+++ project_vue/src/components/PostForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+
+    <!-- =============================Start============================= -->
+    <!-- 2 вариант создания синхронизации с input       @input="title = $event.target.value" 
+    -->
+
+    <!-- <input 
+      v-bind:value="post.title"
+      @input="post.title = $event.target.value"
+      type="text" 
+      class="input" 
+      placeholder="Название">
+      -->
+    <!-- 2 вариант создания синхронизации с input       @input="body = $event.target.value"  (первый находится в script)-->
+    <!-- <input 
+      v-bind:value="post.body"  
+      @input="post.body = $event.target.value"
+      type="text" 
+      class="input" 
+      placeholder="Описание"> -->
+    <!-- ================================End========================= -->
+
+    <!-- Дусторонее связывание с использованием дерективы v-model="post.body "
+      ===================Убрали========================v-bind:value="post.title"
+      ===================Убрали========@input="post.title = $event.target.value" -->
+    <input
+      v-model="post.title"
+      type="text"
+      class="input"
+      placeholder="Название"
+    />
+
+    <input
+      v-model="post.body"
+      type="text"
+      class="input"
+      placeholder="Описание"
+    />
+
+    <!-- Повесили слуштель обытия создания на кнопку @click ="/(создаем функцию)\ createPost" далее добавили поле methods
+     в script компонент -->
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: "",
+        body: "",
+    }
+  }
+  
+}
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+</style>
